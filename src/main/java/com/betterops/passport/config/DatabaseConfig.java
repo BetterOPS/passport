924a3ef0c8f4772e8ccafefacc4f3c62e4f84b66
leoliu <leoliuy90@gmail.com>
2017-07-19 17:44:28 +0800

    add druid

--- src/main/java/com/betterops/passport/config/DatabaseConfig.java
+++ src/main/java/com/betterops/passport/config/DatabaseConfig.java
@@ -1,47 +1,87 @@
 package com.betterops.passport.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.context.annotation.PropertySource;
+
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 
 @Configuration
 @MapperScan("com.betterops.passport.mapper")
 public class DatabaseConfig {
 
-    @Bean
+    @Configuration
     @Profile("dev")
-    public DataSource devDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource("jdbc:mysql://localhost:3306/test",
-                "root", "");
-        return dataSource;
-    }
+    @PropertySource("classpath:application-dev.properties")
+    static class Dev {}
 
-    @Bean
+    @Configuration
     @Profile("prod")
-    public DataSource prodDataSource() {
-        return null;
+    @PropertySource("classpath:application-prod.properties")
+    static class Prod {}
+
+    @Value("${spring.datasource.driverClassName}") String driver;
+    @Value("${spring.datasource.username}") String username;
+    @Value("${spring.datasource.password}") String password;
+    @Value("${spring.datasource.url}") String url;
+    @Value("${spring.datasource.initialSize}") int initialSize;
+    @Value("${publicKey}") String publicKey;
+    @Value("${spring.datasource.minIdle}") int minIdle;
+    @Value("${spring.datasource.maxActive}") int maxActive;
+    @Value("${spring.datasource.maxWait}") long maxWait;
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}") long timeBetweenEvictionRunsMillis;
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}") long minEvictableIdleTimeMillis;
+    @Value("${spring.datasource.validationQuery}") String validationQuery;
+    @Value("${spring.datasource.testWhileIdle}") boolean testWhileIdle;
+    @Value("${spring.datasource.testOnBorrow}") boolean testOnBorrow;
+    @Value("${spring.datasource.testOnReturn}") boolean testOnReturn;
+    @Value("${spring.datasource.poolPreparedStatements}") boolean poolPreparedStatements;
+    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}") int maxPoolPreparedStatementPerConnectionSize;
+    @Value("${spring.datasource.filters}") String filters;
+    @Value("${spring.datasource.connectionProperties}") String connectionProperties;
+
+
+    @Bean
+    public DataSource dataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(driver);
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(username);
+        druidDataSource.setPassword(password);
+        druidDataSource.setInitialSize(initialSize);
+        druidDataSource.setMaxActive(maxActive);
+        druidDataSource.setMaxWait(maxWait);
+        druidDataSource.setTimeBetweenConnectErrorMillis(timeBetweenEvictionRunsMillis);
+        druidDataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        druidDataSource.setValidationQuery(validationQuery);
+        druidDataSource.setTestWhileIdle(testWhileIdle);
+        druidDataSource.setTestOnBorrow(testOnBorrow);
+        druidDataSource.setTestOnReturn(testOnReturn);
+        druidDataSource.setPoolPreparedStatements(poolPreparedStatements);
+        druidDataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+        druidDataSource.setConnectionProperties(connectionProperties);
+        try {
+            druidDataSource.setFilters(filters);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return druidDataSource;
     }
 
     @Bean
-    @Profile("dev")
-    public SqlSessionFactoryBean devSqlSessionFactoryBean() throws Exception {
+    public SqlSessionFactoryBean sqlSessionFactoryBean() throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(devDataSource());
+        sqlSessionFactoryBean.setDataSource(dataSource());
         sqlSessionFactoryBean.setTypeAliasesPackage("com.betterops.passport.domain");
         return sqlSessionFactoryBean;
     }
 
-    @Bean
-    @Profile("prod")
-    public SqlSessionFactoryBean prodSqlSessionFactoryBean() throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(prodDataSource());
-        return sqlSessionFactoryBean;
-    }
 }
